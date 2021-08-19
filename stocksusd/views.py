8bdf4ca910e97ab907d64e60ddee9e20e19190a7
Andres Alvarez <andres15alvarez@gmail.com>
2021-08-19 15:13:03 -0400

    Add query params filter in views

--- stocksusd/views.py
+++ stocksusd/views.py
@@ -1,10 +1,20 @@
 from rest_framework import generics
+from rest_framework import Response
 from stockusd.models import StockUSD
 from stockusd.serializers import StockUSDSerializer
 
 
 class StockUSDView(generics.ListAPIView):
 
-	queryset = StockUSD.objects.all()
 	serializer_class = StockUSD
 	filterset_Fields = ['date_from', 'date_to']
+
+	def get_queryset(self):
+		date_from = self.request.query_params.get('date_from', '')
+		date_to = self.request.query_params.get('date_to', '')
+		return StockUSD.objects.filter(date__range=(date_from, date_to))
+
+	def list(self):
+		queryset = self.get_queryset()
+		serializer = StockUSDSerializer(queryset, many=true)
+		return Response(serializer.data)
--- stocksves/views.py
+++ stocksves/views.py
@@ -5,6 +5,15 @@ from stockves.serializers import StockVESSerializer
 
 class StockVESView(generics.ListAPIView):
 
-	queryset = StockVES.objects.all()
 	serializer_class = StockVES
 	filterset_Fields = ['date_from', 'date_to']
+
+	def get_queryset(self):
+		date_from = self.request.query_params.get('date_from', '')
+		date_to = self.request.query_params.get('date_to', '')
+		return StockVES.objects.filter(date__range=(date_from, date_to))
+
+	def list(self):
+		queryset = self.get_queryset()
+		serializer = StockVESSerializer(queryset, many=true)
+		return Response(serializer.data)
