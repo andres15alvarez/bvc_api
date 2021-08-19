09d36022fa6159c6683ae5df0c609b220ad1bad8
Andres Alvarez <andres15alvarez@gmail.com>
2021-08-19 13:49:26 -0400

    Set up models

--- stocksusd/models.py
+++ stocksusd/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class StockUSD(models.Model):
+
+	code = models.CharField(max_length=5)
+	open = models.DecimalField(max_digits=12, decimal_places=2)
+	close = models.DecimalField(max_digits=12, decimal_places=2)
+	high = models.DecimalField(max_digits=12, decimal_places=2)
+	low = models.DecimalField(max_digits=12, decimal_places=2)
+	date = models.DateField()
+
+	class Meta:
+		db_table = 'stock_usd'
+		ordering = ['-date', 'code']
+		verbose_name = 'stock usd daily'
+		verbose_plural_name = 'stocks usd daily'
+		constraint = [
+			models.UniqueConstraint(fields=['code', 'date'], )
+		]
+
+	def __str__(self):
+		return f'{self.code} O:{self.open} L:{self.low} H:{self.high}'\
+				f'C:{self.close} at {self.date}'
--- stocksves/models.py
+++ stocksves/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class StockVES(models.Model):
+
+	code = models.CharField(max_length=5)
+	open = models.DecimalField(max_digits=12, decimal_places=2)
+	close = models.DecimalField(max_digits=12, decimal_places=2)
+	high = models.DecimalField(max_digits=12, decimal_places=2)
+	low = models.DecimalField(max_digits=12, decimal_places=2)
+	date = models.DateField()
+
+	class Meta:
+		db_table = 'stock_ves'
+		ordering = ['-date', 'code']
+		verbose_name = 'stock ves daily'
+		verbose_plural_name = 'stocks ves daily'
+		constraint = [
+			models.UniqueConstraint(fields=['code', 'date'], )
+		]
+
+	def __str__(self):
+		return f'{self.code} O:{self.open} L:{self.low} H:{self.high}'\
+				f'C:{self.close} at {self.date}'
