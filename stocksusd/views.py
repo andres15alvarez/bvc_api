173581c56ede41b464d0b28ef2049e81d239c2d9
Andres Alvarez <andres15alvarez@gmail.com>
2021-08-19 14:56:43 -0400

    Set up views

--- stocksusd/views.py
+++ stocksusd/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from stockusd.models import StockUSD
+from stockusd.serializers import StockUSDSerializer
 
-# Create your views here.
+
+class StockUSDView(generics.ListAPIView):
+
+	queryset = StockUSD.objects.all()
+	serializer_class = StockUSD
+	filterset_Fields = ['date_from', 'date_to']
--- stocksves/views.py
+++ stocksves/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from stockves.models import StockVES
+from stockves.serializers import StockVESSerializer
 
-# Create your views here.
+
+class StockVESView(generics.ListAPIView):
+
+	queryset = StockVES.objects.all()
+	serializer_class = StockVES
+	filterset_Fields = ['date_from', 'date_to']
