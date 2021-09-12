75668462d76b68c5adb206f31dacab546a8e4a74
Andres Alvarez <andres15alvarez@gmail.com>
2021-09-12 17:46:37 -0400

    Insert ibc data to database

--- fill_database.py
+++ fill_database.py
@@ -11,12 +11,16 @@ Stock = namedtuple(
     'Stock', 
     ['code', 'date', 'volume', 'open', 'high', 'low', 'close']
 )
+Usd = namedtuple('Usd', ['date', 'open', 'high', 'low', 'close'])
+Ibc = namedtuple('Ibc', ['date', 'ibc'])
 codes = [
     'ABCa', 'BNC', 'BOU', 'BPV', 'BVCC', 'BVL', 'CGQ', 'CIE', 'CRMa', 'DOM', 'EFE', 'ENV',
     'FVIa', 'FVIb', 'GZL'
 ]
 rows_ves = []
 rows_usd = []
+rows_usd_exc = []
+rows_ibc = []
 for code in codes:
     with open(f'{BASE_DIR}\\stocks_usd_daily\\{code} Daily data_usd.csv', newline='') as f:
         reader = csv.DictReader(f)
@@ -47,6 +51,31 @@ for code in codes:
                 )
             )
 
+with open(f'{BASE_DIR}\\data\\USD_BS.csv', newline='') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        rows_usd_exc.append(
+            Usd(
+                date=row['Date'],
+                open=float(row['Open']),
+                high=float(row['High']),
+                low=float(row['Low']),
+                close=float(row['Price']),
+            )
+        )
+
+with open(f'{BASE_DIR}\\data\\IBC.csv', newline='') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        date = row['Date'].split('/')
+        date = '-'.join([date[2], date[0], date[1]])
+        rows_ibc.append(
+            Ibc(
+                date=date,
+                ibc=row['IBC']
+            )
+        )
+
 dotenv.read_dotenv()
 conn = psycopg2.connect(
     database=os.environ.get('DATABASE_NAME'),
@@ -57,17 +86,29 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-for row in rows:
+for row in rows_usd:
     cur.execute(
-        'INSERT INTO stock_ves (code, date, volume, open, high, low, close) VALUES (%s, %s, %s, %s, %s, %s, %s);', 
+        'INSERT INTO stock_usd (code, date, volume, open, high, low, close) VALUES (%s, %s, %s, %s, %s, %s, %s);', 
         (row.code, row.date, row.volume, row.open, row.high, row.low, row.close)
     )
         
-for row in rows:
+for row in rows_ves:
     cur.execute(
         'INSERT INTO stock_ves (code, date, volume, open, high, low, close) VALUES (%s, %s, %s, %s, %s, %s, %s);', 
         (row.code, row.date, row.volume, row.open, row.high, row.low, row.close)
     )
+
+for row in rows_usd_exc:
+    cur.execute(
+            'INSERT INTO usd_exchange (date, open, high, low, close) VALUES (%s, %s, %s, %s, %s)',
+            (row.date, row.open, row.high, row.low, row.close)
+        )
+
+for row in rows_ibc:
+    cur.execute(
+            'INSERT INTO ibc_ibc (date, ibc) VALUES (%s, %s)',
+            (row.date, row.ibc)
+        )
 
 conn.commit()
 cur.close()
