73df60cd9089cddbca211f8da68f6cd460529039
Andres Alvarez <andres15alvarez@gmail.com>
2021-09-12 09:54:23 -0400

    Update views to support query params

--- stockusd/views.py
+++ stockusd/views.py
@@ -1,20 +1,36 @@
-from rest_framework import generics
+"""Stock on USD Views."""
+from rest_framework import generics, status
 from rest_framework.response import Response
 from stockusd.models import StockUSD
 from stockusd.serializers import StockUSDSerializer
 
 
 class StockUSDView(generics.ListAPIView):
+	"""List View to get all records of transactions daily given a date range and/or
+		a company code.
 
-	serializer_class = StockUSD
-	filterset_Fields = ['date_from', 'date_to']
+		Query Params:
+			company_code (str): Optional company code (RST)
+			date_from (str): String format YYYY-MM-DD to date (2021-01-01)
+			date_to (str): String format YYYY-MM-DD to date (2021-01-01)
+
+	"""
 
 	def get_queryset(self):
-		date_from = self.request.query_params.get('date_from', '')
-		date_to = self.request.query_params.get('date_to', '')
-		return StockUSD.objects.filter(date__range=(date_from, date_to))
+		company_code = self.request.query_params.get('company_code', '')
+		date_from = self.request.query_params.get('date_from')
+		date_to = self.request.query_params.get('date_to')
+		if date_from and date_to:
+			return StockUSD.objects.filter(
+				date__range=(date_from, date_to), 
+				code=company_code
+			)
+		raise Exception('query params null')
 
-	def list(self):
-		queryset = self.get_queryset()
-		serializer = StockUSDSerializer(queryset, many=true)
-		return Response(serializer.data)
+	def list(self, *args, **kwargs):
+		try:
+			queryset = self.get_queryset()
+			serializer = StockUSDSerializer(queryset, many=True)
+			return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+		except Exception as e:
+			return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
