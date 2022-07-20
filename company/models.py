8191584a9ffb5d2bae620dd2e1e2f6885cea67bb
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-20 19:56:51 -0400

    Update company model

--- company/models.py
+++ company/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Company(models.Model):
 
-	name = models.CharField(max_length=100, null=False, blank=False)
+	name = models.CharField(max_length=100, null=True)
 	code = models.CharField(max_length=5, null=False, unique=True)
 
 	class Meta:
