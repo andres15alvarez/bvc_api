0c7c6c6068aa41481593d1288b32e59e0004963a
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-20 18:42:12 -0400

    Create serializer for date range

--- ibc/serializers.py
+++ ibc/serializers.py
@@ -7,3 +7,8 @@ class IBCSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = IBC
 		exclude = ['id']
+
+
+class IBCRangeSerializer(serializers.Serializer):
+	date_from = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d"])
+	date_to = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d"])
