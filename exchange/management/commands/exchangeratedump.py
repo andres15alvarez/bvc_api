f7e7ee875ee6e7fcdeba49050d818c9a32ab8c1e
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-20 19:57:37 -0400

    Create commands to dump data in the database

---
+++ exchange/management/commands/exchangeratedump.py
@@ -0,0 +1,50 @@
+import csv
+from datetime import date
+from django.apps import apps
+from django.core.management.base import BaseCommand, no_translations, CommandError
+from exchange.models import ExchangeRate
+
+
+class Command(BaseCommand):
+
+    help = "Dump in the database the records of the exchange rate USD-VES"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--csv', nargs=1, type=str)
+
+    @no_translations
+    def handle(self, *args, **options):
+        if 'csv' not in options:
+            raise CommandError('CSV File is needed!')
+        list_exchange_rate = []
+        currency = apps.get_model('currency', 'Currency')
+        usd_currency = currency.objects.get_or_create(
+            code="USD",
+            name="United States Dollar"
+        )
+        ves_currency = currency.objects.get_or_create(
+            code="VES",
+            name="Bolivar"
+        )
+        with open(options['csv'], newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                date_splitted = row['Date'].split('/')
+                list_exchange_rate.append(
+                    ExchangeRate(
+                        date=date(
+                            int(date_splitted[2]),
+                            int(date_splitted[0]),
+                            int(date_splitted[1])
+                        ),
+                        open=float(row['Open']),
+                        high=float(row['High']),
+                        low=float(row['Low']),
+                        close=float(row['Price']),
+                        from_currency=usd_currency,
+                        to_currency=ves_currency
+                    )
+                )
+        objects_created = ExchangeRate.objects.bulk_create(list_exchange_rate)
+        self.stdout(self.style.SUCCESS(f"{len(objects_created)} rows inserted"))
+
---
+++ stock/management/commands/stockdump.py
@@ -0,0 +1,44 @@
+import csv
+from datetime import date
+from django.apps import apps
+from django.core.management.base import BaseCommand, no_translations, CommandError
+from stock.models import Stock
+
+
+class Command(BaseCommand):
+
+    help = "Dump in the database the records of the stocks"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--csv', nargs=1, type=str)
+        parser.add_argument('--code', nargs=1, type=str)
+
+    @no_translations
+    def handle(self, *args, **options):
+        if 'csv' not in options:
+            raise CommandError('CSV File is needed!')
+        if 'code' not in options:
+            raise CommandError('Code of the stock is needed!')
+        with open(options['csv'], newline='') as f:
+            reader = csv.DictReader(f)
+            list_stock = []
+            company = apps.get_model('company', "Company")
+            company_stock = company.objects.get_or_create(
+                code=options["code"]
+            )
+            for row in reader:
+                date_splitted = row['Date'].split('/')
+                list_stock.append(
+                    Stock(
+                        code=company_stock,
+                        date=date(int(date_splitted[2]), int(date_splitted[0]), int(date_splitted[1])),
+                        volume= int(float(row['Volumen'])),
+                        open=float(row['Apertura Usd/Bs']),
+                        high=float(row['Alto Usd/Bs']),
+                        low=float(row['Bajo Usd/Bs']),
+                        close=float(row['Cierre Usd/Bs']),
+                    )
+                )
+        objects_created = Stock.objects.bulk_create(list_stock)
+        self.stdout(self.style.SUCCESS(f"{len(objects_created)} rows inserted"))
+
