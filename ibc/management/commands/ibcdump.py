3a6a0c6acc6bd55c4a65f56df04370a64242eb02
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-20 18:42:55 -0400

    Create command to dump ibc data

---
+++ ibc/management/commands/ibcdump.py
@@ -0,0 +1,31 @@
+import csv
+from datetime import date
+from django.core.management.base import BaseCommand, no_translations, CommandError
+from ibc.models import IBC
+
+
+class Command(BaseCommand):
+
+    help = "Dump in the database the records of the IBC"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--csv', nargs=1, type=str)
+
+    @no_translations
+    def handle(self, *args, **options):
+        if 'csv' not in options:
+            raise CommandError('CSV File is needed!')
+        with open(options['csv'], newline='') as f:
+            reader = csv.DictReader(f)
+            list_ibc = []
+            for row in reader:
+                date_splitted = row['Date'].split('/')
+                list_ibc.append(
+                    IBC(
+                        date=date(int(date_splitted[2]), int(date_splitted[0]), int(date_splitted[1])),
+                        ibc=row['IBC']
+                    )
+                )
+        objects_created = IBC.objects.bulk_create(list_ibc)
+        self.stdout(self.style.SUCCESS(f"{len(objects_created)} rows inserted"))
+
