f050e679114a0e837ee8e6b0e9999aa29903c002
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-20 21:16:13 -0400

    Create serializer for date range

--- apps/exchange/serializers.py
+++ apps/exchange/serializers.py
@@ -7,3 +7,8 @@ class ExchangeRateSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ExchangeRate
 		exclude = ['id']
+
+
+class ExchangeRateRangeSerializer(serializers.Serializer):
+	date_from = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d"])
+	date_to = serializers.DateField(format="%Y-%m-%d", input_formats=["%Y-%m-%d"])
