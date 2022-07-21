ad24e4f7ecf6d492ff7b5f1f734113984540788e
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-20 22:06:49 -0400

    Create tests for exchange list view

--- apps/exchange/tests.py
+++ apps/exchange/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_list_ibc(client):
+    response = client.get('/exchange', data={'date_from': '2021-01-01', 'date_to': '2021-01-05'})
+    assert response.status_code == 200
