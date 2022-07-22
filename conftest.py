629ce95589a8567aded5bef5c1cbd6b405e45c90
Andres Alvarez <andres15alvarez@gmail.com>
2022-07-22 16:41:10 -0400

    Add conftest to use an existing database

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+from django.conf import settings
+
+
+@pytest.fixture(scope='session')
+def django_db_setup():
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DATABASE_HOST', ''),
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'PORT': os.environ.get('DATABASE_PORT', 5432),
+    }
